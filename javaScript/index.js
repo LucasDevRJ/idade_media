57575c1a5f694ba98584553d0e9f73487266fa43
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-14 14:49:15 -0300

    Lógica com repetição para inserir idades de familiares

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,18 @@
 function criaElemento() {
-	var familiar = document.getElementById("familiar");
-	var texto = document.createElement("p");
-	var entrada = document.createElement("input");
-	familiar.appendChild(texto);
-	familiar.appendChild(entrada);
+	var campoQuantidadeMembrosFamilia = document.getElementById("entrada");
+
+	var quantidadeMembrosFamilia = campoQuantidadeMembrosFamilia.value;
+
+	for (var contador = 0; contador < quantidadeMembrosFamilia; contador++) {
+		var familiar = document.getElementById("familiar");
+		var paragrafo = document.createElement("p");
+		var entrada = document.createElement("input");
+
+		var texto  = document.createTextNode("Digite a idade do famíliar: ");
+
+		paragrafo.appendChild(texto);
+
+		familiar.appendChild(texto);
+		familiar.appendChild(entrada);
+	} 
 }
